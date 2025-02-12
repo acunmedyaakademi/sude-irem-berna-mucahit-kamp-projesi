ddf24071e07509cd9a12a5948e56b23859a8a6df
İrem Çoban <iremcoban@192.168.1.107>
2025-02-12 22:02:10 +0300

    hamburger menu css yapıldı

--- assets/js/product-detail.js
+++ assets/js/product-detail.js
@@ -1,32 +1,240 @@
 async function getData() {
-  const data = await fetch('../../data.json').then(r => r.json());
+  const data = await fetch("../../data.json").then((r) => r.json());
   renderCategories(data);
   const product = location.href.split("#")[1].slice(1);
-  const selectedProduct = data.find(x => x.slug === product);
+  const selectedProduct = data.find((x) => x.slug === product);
   renderProduct(selectedProduct);
 }
 
-function renderCategories(categories){
-  const categoriesContainer = document.querySelector('.categories');
-  const categoryNames = new Set(categories.map(x => x.category));
+function init(){
+  const cartIcon = document.querySelector('.cart-icon');
+  cartIcon.addEventListener('click', showCart);
+  goBackBtn.addEventListener('click' ,  goBack);
+  getData();
+  cartDialog.addEventListener('click', (e) => {
+    const cartContainer = document.querySelector('.cart-dialog-container');
+    const isInsideCart = cartContainer.contains(e.target);
+    const isPlusOrMinus = e.target.classList.contains('plus-icon') || e.target.classList.contains('minus-icon');
+  
+    if (!isInsideCart && !isPlusOrMinus) {
+      cartDialog.close();
+    }
+  });
+  hamburgerMenuBtn.addEventListener('click', showHamburgerMenu);
+}
+
+function renderCategories(categories) {
+  const categoriesContainer = document.querySelector(".categories");
+  const categoryNames = new Set(categories.map((x) => x.category));
   let categoryItems = [];
   for (const x of categoryNames) {
-    categoryItems.push(categories.find(category => category.category === x));
+    categoryItems.push(categories.find((category) => category.category === x));
   }
-  categoriesContainer.innerHTML = categoryItems.map(x => 
-    `
+    categoriesContainer.innerHTML = categoryItems.map(
+    (x) =>
+      `
       <div class="category-item">
         <img class="category-img" src="..${x.categoryImage.svg}" alt="Speakers Img">
         <h3>${x.category}</h3>
         <a id="speakers-pages" href="../pages/${x.category}.html">Shop<img src="/assets/home/mobile/right.svg" alt=""></a>
       </div>
     `
-  )
+  );
+  hamburgerMenu.innerHTML = categoryItems.map(x => 
+    `
+      <div class="category-item">
+        <img class="category-img" src="..${x.categoryImage.svg}" alt="Speakers Img">
+        <h3>${x.category}</h3>
+        <a id="speakers-pages" href="./${x.category}.html">Shop<img src="/assets/home/mobile/right.svg" alt=""></a>
+      </div>
+    `
+  ).join("");
 }
 
-function renderProduct(product){
+function renderProduct(product) {
+  const productDetailItem = document.querySelector(".product-detail-item");
+  productDetailItem.innerHTML = `
+        <img class="icategory-page-preview__mobile" src="..${
+          product.image.mobile
+        }" alt="Product Img">
+        <img src="..${product.image.desktop}" alt="">
+        <div class="product-detail-text">
+          ${product.isNew ? " <h4>New Product</h4> " : ""}
+          <h3>${product.name}</h3>
+          <p>${product.description}</p>
+          <h5>$ ${product.price}</h5>
+          <div class="product-detail-text__button">
+            <div class="quantity">
+              <button id="decreaseBtn">−</button>
+              <span id="quantityBtn">1</span>
+              <button id="increaseBtn">+</button>
+          </div>
+            <a id="addToCartBtn" class="btn" href="#">Add to cart</a>
+          </div>
+  `;
 
+  increaseBtn.addEventListener('click', () => {
+   let newQuantity = Number(quantityBtn.innerText) + 1;
+   quantityBtn.innerText = newQuantity;
+  });
+
+  decreaseBtn.addEventListener('click', () => {
+    if(Number(quantityBtn.innerText) !== 1) {
+      let newQuantity = Number(quantityBtn.innerText) - 1;
+      quantityBtn.innerText = newQuantity;
+    }
+  })
+  
+  addToCartBtn.addEventListener("click", (e) => addToCart(e, product));
+
+  
+
+  const productFeatures = document.querySelector(".product-features");
+  productFeatures.innerHTML = `
+        <h2>Features</h2>
+        <p class="product-features-text">
+        ${product.features}
+        </p>
+        <div class="product-features-box">
+          <h2>In the box</h2>
+          <div class="product-features-box__item">
+            ${product.includes
+              .map((x) => `<span><p>${x.quantity}x</p><p>${x.item}</p></span>`)
+              .join("")}
+          </div>
+        </div>
+  `;
+
+  const productDetailImages = document.querySelector(".product-detail-images");
+  productDetailImages.innerHTML = product.gallery.map(
+    (x) => `
+  <img  src="..${x.desktop}" alt="1x">
+  `
+  );
+
+  const productAlsoLike = document.querySelector(".product-also-like");
+  productAlsoLike.innerHTML = product.others.map(
+    (x) => `
+     <div class="product-also-like-item">
+          <img src="..${x.image.desktop}" alt="">
+          <h3>${x.name}</h3>
+          <a id="productBtn" href="product-detail.html#/${x.slug}">See Product</a>
+        </div>
+    `
+  ).join('');
+  window.addEventListener("hashchange", function () {
+    location.reload();
+    window.scrollTo(0, 0);
+  });
+  
 }
 
+function addToCart(e, product) {
+  e.preventDefault();
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const selectedProductIndex = cart.findIndex((x) => x.name === product.name);
+  if (selectedProductIndex === -1) {
+    cart.push({
+      name: product.name,
+      image: product.image.desktop,
+      price: product.price,
+      quantity: Number(quantityBtn.innerText),
+    });
+  } else {
+    cart[selectedProductIndex].quantity += Number(quantityBtn.innerText);
+  }
+  localStorage.cart = JSON.stringify(cart);
+  console.log(cart);
+}
 
-getData();
+function goBack(e) {
+  e.preventDefault();
+  window.history.back();
+}
+
+function showCart(e){
+  e.preventDefault();
+  cartDialog.showModal();
+  renderCart();
+}
+
+function renderCart(){
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let totalPrice = 0;
+  let totalItems = 0;
+  for (const item of cart) {
+    totalPrice = totalPrice + (item.price * item.quantity);
+    totalItems = totalItems + item.quantity;
+  }
+  cartDialog.innerHTML = `
+    <div class="cart-dialog-container">
+      <div class="cart-dialog-container-top">
+        <h3>CART(<span>${totalItems}</span>)</h3>
+        <button id="removeAllBtn">Remove all</button>
+      </div>
+      <div class="cart-items">
+        ${cart.map(x => `
+            <div class="cart-item">
+              <div class="cart-item-wrapper">
+                <img src="..${x.image}">
+                <div class="cart-item-wrapper-text">
+                  <h3>${(x.name)}</h3>
+                  <p>$ ${(x.price) }</p>
+                </div>
+              </div>
+              <div class="quantity-btn">
+                <p class="minus-icon" data-name="${x.name}">-</p>
+                <p class="quantity">${x.quantity}</p>
+                <p class="plus-icon" data-name="${x.name}">+</p>
+              </div>
+            </div>
+          `)}
+      </div>
+      <div class="total-price">
+        <p>TOTAL</p>
+        <p class="price">$ ${totalPrice}</p>
+      </div>
+      <a href="checkout.html" class="btn">CHECKOUT</a>
+    </div>
+  `
+  const minusIcons = document.querySelectorAll('.minus-icon');
+  for (const minusIcon of minusIcons) {
+    minusIcon.addEventListener('click', (e) => {
+      const selectedItem = cart.findIndex(x => x.name === e.target.dataset.name);
+      if(cart[selectedItem].quantity === 1){
+        cart.splice(selectedItem, 1);
+        localStorage.cart = JSON.stringify(cart);
+        renderCart();
+      } else{
+        cart[selectedItem].quantity = cart[selectedItem].quantity - 1;
+        localStorage.cart = JSON.stringify(cart);
+        renderCart();
+      }
+    })
+  }
+
+  const plusIcons = document.querySelectorAll('.plus-icon');
+  for (const plusIcon of plusIcons) {
+    plusIcon.addEventListener('click', (e) => {
+      const selectedItem = cart.findIndex(x => x.name === e.target.dataset.name);
+      cart[selectedItem].quantity = cart[selectedItem].quantity + 1;
+      localStorage.cart = JSON.stringify(cart);
+      renderCart();
+    })
+  }
+
+  removeAllBtn.addEventListener('click', () => {
+    if(confirm('Silmek istediğinize emin misiniz?')){
+      localStorage.cart = JSON.stringify([]);
+      cartDialog.close();
+    }
+  })
+}
+
+function showHamburgerMenu(e){
+  e.preventDefault();
+  hamburgerMenu.showModal();
+}
+
+init();
+
